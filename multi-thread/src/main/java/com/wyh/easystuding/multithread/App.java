2f1c404679faecd5f01e1b94f3b1db6da690a6df
wangyonghua <yonghuawang3@creditease.cn>
2019-12-09 19:22:28 +0800

    初始化码出高效项目

---
+++ multi-thread/src/main/java/com/wyh/easystuding/multithread/App.java
@@ -0,0 +1,11 @@
+package com.wyh.easystuding.multithread;
+
+/**
+ * @author : wangyonghua
+ * @date Date : 2019/12/9 19:16
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
